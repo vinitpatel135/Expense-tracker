c01afb7164da2fd2cedc1c2874c20061cb3329b9
vinitpatel135 <patelvinit135@gmail.com>
2024-10-09 00:13:14 +0530

    first commit

---
+++ backend/DB/Database.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+export const connectDB = async (req, res) => {
+    const db = process.env.MONGO_URL
+ 
+    const {connection} = await mongoose.connect(db, { useNewUrlParser: true });
+
+
+    console.log(`MongoDB Connected to ${connection.host}`);
+
+}
